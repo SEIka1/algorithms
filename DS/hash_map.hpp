d3cb59680ca8b1cea722b5293bd0f485d38dc469
SEIka69 <52436100+SEIka1@users.noreply.github.com>
2025-04-03 17:18:47 +0300

    Update hash_map.hpp
    added constructor and func - rehash_if_need which do rehash if load_factor > maximum_load_factor. Used [[no_unique_address]] for allocator.

--- DS/hash_map.hpp
+++ DS/hash_map.hpp
@@ -12,14 +12,14 @@
 #include <string_view>
 
 template<typename Hash, typename Key>
-concept ValidHasher = requires(Hash h, Key k) {
-  { h(k) } -> std::convertible_to<std::size_t>;
+concept ValidHasher = requires(Hash h_, Key k_) {
+  { h_(k_) } -> std::convertible_to<std::size_t>;
 };
 
 template <typename Equal, typename Key>
-concept TransparentEqual = requires(Equal eq, Key k, std::string_view s) {
-    { eq(k, k) } -> std::convertible_to<bool>;
-    { eq(k, s) } -> std::convertible_to<bool>;
+concept TransparentEqual = requires(Equal eq_, Key k_, std::string_view s_) {
+    { eq_(k_, k_) } -> std::convertible_to<bool>;
+    { eq(k_, s_) } -> std::convertible_to<bool>;
 };
 
 template <
@@ -28,5 +28,30 @@ template <
     typename Hash = std::hash<Key>,
     typename KeyEqual = std::equal_to<>,
     typename Allocator = std::allocator<std::pair<const Key, Value>>
-> requires ValidHasher<Hash, Key> && TransparentEqual<KeyEqual, Key>
-class hash_map;
+>   requires ValidHasher<Hash, Key> && TransparentEqual<KeyEqual, Key>
+class hash_map {
+private:
+    using Bucket = std::list<std::pair<const Key, Value>, Allocator>;
+    std::vector<Bucket> buckets;
+    Hash hash_fn;
+    KeyEqual key_eq;
+    [[no_unique_address]] Allocator alloc;
+    float max_load_factor = 0.75f;
+
+    void rehash_if_needed() {
+        if (load_factor() > max_load_factor) {
+            rehash(buckets.size() * 2);
+        }
+    }
+
+public:
+    using key_type = Key;
+    using mapped_type = Value;
+    using value_type = std::pair<const Key, Value>;
+    using size_type = std::size_t;
+
+    hash_map(size_type bucket_count = 16, 
+             const Hash& hash = Hash(),
+             const KeyEqual& equal = KeyEqual(),
+             const Allocator& alloc = Allocator())
+        : buckets(bucket_count), hash_fn(hash), key_eq(equal), alloc(alloc) {}
